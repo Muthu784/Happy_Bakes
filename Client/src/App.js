8c08d6fd5ba4cd9f171300f318f4e8a5c8538238
Muthu784 <amuthupandi782@gmail.com>
2025-06-18 19:10:44 +0530

    Initial commit

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
 import Products from './Pages/Products.jsx';
 import LoginComponent from './Pages/Login.jsx';
@@ -13,26 +13,38 @@ import Register from './Pages/Register.jsx';
 import Admin from './Pages/Admin.jsx';
 import { UserProvider } from './context/UserContext';
 
+
+const AppContent = () => {
+  const location = useLocation();
+  const isAuthPage = ['/login', '/register', '/forgotpassword', '/verifyotp', '/resetpassword'].includes(location.pathname);
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/userorder" element={<UserOrder />} />
+          <Route path="/Admin" element={<Admin/>} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/verifyotp" element={<VerifyOTP />} />
+          <Route path="/resetpassword" element={<ResetPassword />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+      {!isAuthPage && <Footer />}
+    </>
+  );
+};
+
 function App() {
   return (
     <UserProvider>
       <BrowserRouter>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="/login" element={<LoginComponent />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/userorder" element={<UserOrder />} />
-            <Route path="/Admin" element={<Admin/>} />
-            <Route path="/forgotpassword" element={<ForgotPassword />} />
-            <Route path="/verifyotp" element={<VerifyOTP />} />
-            <Route path="/resetpassword" element={<ResetPassword />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
+        <AppContent />
       </BrowserRouter>
     </UserProvider>
   );
